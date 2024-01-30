20a4901eb80d1fe4389a64f50b1493612d44ef0f
форк бомба <grigorev@niuitmo.ru>
2024-01-30 16:17:20 +0300

    first version.

---
+++ dmp.c
@@ -0,0 +1,98 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/bio.h>
+#include <linux/device-mapper.h>
+
+#include <linux/fs.h>
+
+struct dmp_data {
+	struct dm_dev *dev;	/* bdev, с которым связан наш mapped device */
+};
+
+/* обработка bio запросов */
+static int dmp_map(struct dm_target *ti, struct bio *bio)
+{
+	struct dmp_data *data = (struct dmp_data *) ti->private;
+	bio->bi_bdev = data->dev->bdev;
+
+	if((bio->bi_opf & WRITE) == WRITE)
+		printk(KERN_DEBUG "\n dmp: bio is a write request.... \n");
+	else
+		printk(KERN_DEBUG "\n dmp: bio is a read request.... \n");
+	
+	submit_bio(bio);
+	
+	printk(KERN_CRIT "\n DMP: DMP_MAP IS FUCKING WORKING \n");
+	return DM_MAPIO_SUBMITTED;
+}
+
+/* конструктор */
+static int dmp_ctr(struct dm_target *ti, unsigned int argc, char **argv)
+{
+	if (argc != 1) {
+		ti->error = "Invalid argument count";
+		return -EINVAL;
+	}
+	
+	struct dmp_data *data;
+	int ret;
+
+	data = kmalloc(sizeof(struct dmp_data), GFP_KERNEL);
+	if(data == NULL)
+	{
+		ti->error = "Cannot allocate";
+		return -ENOMEM;
+	}       
+
+	ret = dm_get_device(ti, argv[0], dm_table_get_mode(ti->table), &data->dev);
+	if (ret) {
+		ti->error = "Device lookup failed";
+		goto bad;
+	}
+
+	ti->private = data;
+	return 0;
+
+bad:
+	kfree(data);
+	return ret;
+}
+
+/* деструктор */
+static void dmp_dtr(struct dm_target *ti)
+{
+	struct dmp_data *data = (struct dmp_data *) ti->private;
+	dm_put_device(ti, data->dev);
+	kfree(data);
+}
+
+static struct target_type target_dmp = {
+	.name = "dmp",
+	.version = {1, 0, 0},
+	.module = THIS_MODULE,
+	.ctr = dmp_ctr,
+	.dtr = dmp_dtr,
+	.map = dmp_map,
+};
+
+
+static int init_dmp(void)
+{
+  int result;
+  result = dm_register_target(&target_dmp);
+  if(result < 0)
+    printk(KERN_CRIT "\n Error in registering target \n");
+  return 0;
+}
+
+static void cleanup_dmp(void)
+{
+  dm_unregister_target(&target_dmp);
+}
+
+module_init(init_dmp);
+module_exit(cleanup_dmp);
+
+MODULE_AUTHOR("<grigorev@niuitmo.ru>");
+MODULE_LICENSE("GPL");
