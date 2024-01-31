3e9f2a1755f2e0dc25e9958894b38df33c3ceeb5
форк бомба <grigorev@niuitmo.ru>
2024-01-31 22:19:15 +0300

    it works!

--- dmp.c
+++ dmp.c
@@ -4,11 +4,26 @@
 #include <linux/bio.h>
 #include <linux/device-mapper.h>
 
-/*#include <linux/fs.h>*/
+#include <linux/fs.h>
+#include <linux/kobject.h>
+#include <linux/string.h> 
+#include <linux/sysfs.h> 
 
 struct dmp_data {
 	struct dm_dev *dev;	/* bdev, с которым связан наш mapped device */
+	unsigned int reads;
+	unsigned int writes;
 };
+
+static int test_data = 10;
+
+static ssize_t volumes_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf) 
+{
+	printk(KERN_CRIT "\n\nVERY IMPORTANT!!! VOLUMES_SHOW IN\n\n");
+	int res = sysfs_emit(buf, "%d\n", test_data);
+	printk(KERN_CRIT "\n\nsysfs_emit_len: %d\n\n", res);
+	return res;
+}
 
 /* обработка bio запросов */
 static int dmp_map(struct dm_target *ti, struct bio *bio)
@@ -17,21 +32,22 @@ static int dmp_map(struct dm_target *ti, struct bio *bio)
 	
 	bio_set_dev(bio, data->dev->bdev);
 	
-	bool flush_bio = (bio->bi_opf & REQ_PREFLUSH);
-	bool fua_bio = (bio->bi_opf & REQ_FUA);
-	bool discard_bio = (bio_op(bio) == REQ_OP_DISCARD);
-	bool meta_bio = (bio->bi_opf & REQ_META);
+	bool write = (bio_data_dir(bio) == WRITE);
+	bool read = (bio_data_dir(bio) == READ);
+	
+	if (write) {
+		printk(KERN_DEBUG "dmp: write request\n");
+		data->writes = data->writes + 1;
+		printk(KERN_CRIT "dmp: writes: %u\n\n", data->writes);
+	}
 
-	printk(KERN_CRIT "meta: %u\n", meta_bio);
-	/*printk(KERN_CRIT "bi_flags: %u\n", bio->bi_flags);*/
-	printk(KERN_CRIT "bv_len: %u\n", bio->bi_io_vec->bv_len);
-	printk(KERN_CRIT "bi_status: %u\n", bio->bi_status);
-	
-	/*if((bio->bi_opf & WRITE) == WRITE)
-		printk(KERN_DEBUG "\n dmp: bio is a write request.... \n");
-	else
-		printk(KERN_DEBUG "\n dmp: bio is a read request.... \n");*/
-	
+	if (read) {
+		printk(KERN_DEBUG "dmp: read request\n");
+		data->reads = data->reads + 1;
+		printk(KERN_DEBUG "dmp: reads: %u\n\n", data->reads);
+		/*запись числа в sysfs*/
+	}
+
 	return DM_MAPIO_REMAPPED;
 }
 
@@ -58,9 +74,13 @@ static int dmp_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 		ti->error = "Device lookup failed";
 		goto bad;
 	}
-
-	ti->private = data;
+		
 	printk(KERN_CRIT " DMP_CTR: finish \n");
+	
+	data->writes = 0;
+	data->reads = 0;
+	ti->private = data;
+
 	return 0;
 
 bad:
@@ -73,6 +93,8 @@ static void dmp_dtr(struct dm_target *ti)
 {
 	struct dmp_data *data = (struct dmp_data *) ti->private;
 	dm_put_device(ti, data->dev);
+	/*kobject_put(data->kobj_dir);
+	sysfs_remove_file(kernel_kobj, data->volumes_attr.attr);*/
 	kfree(data);
 }
 
@@ -88,11 +110,17 @@ static struct target_type target_dmp = {
 
 static int init_dmp(void)
 {
-  int result;
-  result = dm_register_target(&target_dmp);
-  if(result < 0)
-    printk(KERN_CRIT "\n Error in registering target \n");
-  return 0;
+	int result;
+	result = dm_register_target(&target_dmp);
+	if(result < 0)
+		printk(KERN_CRIT "\n Error in registering target \n");
+
+	static struct kobj_attribute volumes_attr = __ATTR_RO(volumes);
+	struct kobject *dmp_object = kobject_create_and_add("dmp", kernel_kobj);
+	
+	int error = sysfs_create_file(dmp_object, &volumes_attr.attr);
+
+	return error;
 }
 
 static void cleanup_dmp(void)
